e28409fc0a87ea14f69e569ebee73c91f419fe3c
unicorn-84 <dev.kapitonov@gmail.com>
2018-02-21 16:12:39 +0300

    Added requester

---
+++ massMedia.js
@@ -0,0 +1,53 @@
+module.exports = [
+  {
+    name: 'ria',
+    brand: 'РИА Новости',
+    url: 'https://ria.ru',
+  },
+  {
+    name: 'tass',
+    brand: 'ТАСС',
+    url: 'https://tass.ru',
+  },
+  {
+    name: 'regnum',
+    brand: 'REGNUM',
+    url: 'https://regnum.ru',
+  },
+  {
+    name: 'interfax',
+    brand: 'Интерфакс',
+    url: 'https://interfax.ru',
+  },
+  {
+    name: 'rosbalt',
+    brand: 'РОСБАЛТ',
+    url: 'http://rosbalt.ru',
+  },
+  {
+    name: 'korrespondent',
+    brand: 'Корреспондент.net',
+    url: 'https://korrespondent.net',
+  },
+  {
+    name: 'radioSvoboda',
+    brand: 'Радио Свобода',
+    url: 'https://svoboda.org',
+  },
+  {
+    name: 'prime',
+    brand: 'ПРАЙМ',
+    url: 'https://1prime.ru',
+  },
+  {
+    name: 'fontanka',
+    brand: 'Фонтанка',
+    url: 'http://www.fontanka.ru',
+  },
+  {
+    name: 'rt',
+    brand: 'RT',
+    url: 'https://russian.rt.com',
+  },
+];
+
--- spider.js
+++ spider.js
@@ -1,7 +1,21 @@
 const db = require('./libs/db');
 const logger = require('./libs/logger');
+const req = require('./libs/requester');
+const massMedia = require('./massMedia');
 
 logger.log('Spider started');
+
+function toDownload(cb) {
+  massMedia.forEach((item) => {
+    req.toDownload(item.url, (error, body) => {
+      if (error) {
+        cb(error);
+        return;
+      }
+      cb(null);
+    });
+  });
+}
 
 db.connectToDb('newsbucket', (error, dbObject) => {
   if (error) {
@@ -9,4 +23,9 @@ db.connectToDb('newsbucket', (error, dbObject) => {
     return;
   }
   logger.log('Connect to mLab');
+  toDownload((err) => {
+    if (err) {
+      logger.log(err);
+    }
+  });
 });
